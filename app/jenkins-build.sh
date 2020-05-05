5d3a6e18a3c5e022c4ed49aae02ba85b407f958b
brucedong <542214695@qq.com>
2020-05-05 17:25:47 +0800

    wwwwwww

--- app/jenkins-build.sh
+++ app/jenkins-build.sh
@@ -29,13 +29,8 @@ echo Completed pushing Docker image. Deploying Docker image to AWS Fargate on `d
 echo start to deploy aws farget .......
 chmod 777  ecs-deploy
 
-./ecs-deploy -c cicdcluster -n cicdservice -d cicdeservice -r us-east-1 -p default -i 82651270708.dkr.ecr.us-east-1.amazonaws.com/cicd:latest 
+./ecs-deploy -c cicdcluster -n cicdservice  -r us-east-1 -p default -i 82651270708.dkr.ecr.us-east-1.amazonaws.com/cicd:latest 
 
-if [$? -ne 0 ];then
-echo "failed"
-else
-echo "success"
-fi
 
 echo start to deploy aws farget successfully
 
