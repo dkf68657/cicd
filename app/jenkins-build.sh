81e7cdebaf7c02333619cf5c607b066a960ac878
brucedong <542214695@qq.com>
2020-05-05 16:50:38 +0800

    code commit 10

--- app/jenkins-build.sh
+++ app/jenkins-build.sh
@@ -22,8 +22,13 @@ echo ----Logging in to Amazon ECR successfully------
 
 echo start push docker image to ecr
 
-#sudo docker push 082651270708.dkr.ecr.us-east-1.amazonaws.com/cicd:latest
+sudo docker push 082651270708.dkr.ecr.us-east-1.amazonaws.com/cicd:latest
 
 echo Completed pushing Docker image. Deploying Docker image to AWS Fargate on `date`
 
-printf '[{"name":"cicdservice","imageUri":"%s"}]' 082651270708.dkr.ecr.us-east-1.amazonaws.com/cicd:latest > ../imagedefinitions.json
+echo start to deploy aws farget .......
+
+ecs-deploy -c cicdcluster -n cicdservice -i 82651270708.dkr.ecr.us-east-1.amazonaws.com/cicd:latest
+
+echo start to deploy aws farget successfully
+
