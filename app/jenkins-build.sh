a0c8f5659338e85bc34cf5f482c8fe5bdc323ee4
brucedong <542214695@qq.com>
2020-05-05 22:05:46 +0800

    jjkkwwww

--- app/jenkins-build.sh
+++ app/jenkins-build.sh
@@ -20,16 +20,17 @@ sudo docker login
 
 echo ----Logging in to docker hub successfully------
 
-echo start push docker image to docker hub
+echo -------start push docker image to docker hub------
 
 sudo docker push brucedong1987/cicd:latest
 
-echo Completed pushing Docker image. 
+echo Completed pushing Docker image ----. 
 echo Deploying Docker image to AWS Fargate on `date`
 
 chmod 766  ecs-deploy
 
-./ecs-deploy -c cicdcluster -n cicdservice  -r us-east-1 -p default  
+./ecs-deploy -c cicdcluster -n cicdservice  -r us-east-1 -p default  -i brucedong1987/cicd:latest
+
 
 echo start to deploy aws farget successfully
 
