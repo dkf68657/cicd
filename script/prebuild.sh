626ccde2e19a68305cf41027c8b1a71b78a863bb
brucedong <542214695@qq.com>
2020-05-06 15:29:05 +0800

    wewewewwe

--- script/prebuild.sh
+++ script/prebuild.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
- getConfigValue()
- {
-  local result = `cat config.json | jq $1`
+getConfigValue()
+{
+  local result = $(cat config.json | jq .$1)
   echo $result
-  return 0
- }
+}
 
 current_dir="$PWD"
 generated_dir="$current_dir/generated"
@@ -20,7 +19,7 @@ cp -r $template_dir/*   $generated_dir
 echo "replace placehold in $generated_dir/task-definition.json"  
 
 
-task_family=$(getConfigValue ".task.family")
+task_family=$(getConfigValue task.family)
 echo $task_family
 
 replace="s/%%FAMILY%%/$task_family/g" 
