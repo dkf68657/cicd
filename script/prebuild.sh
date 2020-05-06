f9c9b438c3d8f1c6106cf05321fb92b18bb4bc21
brucedong <542214695@qq.com>
2020-05-06 14:06:52 +0800

    wwwewe

--- script/prebuild.sh
+++ script/prebuild.sh
@@ -10,9 +10,9 @@ mkdir -p $generated_dir
 cp -r $template_dir/*   $generated_dir
 
 echo "replace placehold in $generated_dir/task-definition.json"  
-family = `cat config.json | jq .task.family`
-replace = "s/%%FAMILY%%/$family/g" 
-sed -i -e $replace "$generated_dir/task-definition.json"
+task_family=`cat config.json | jq .task.family`
+replace="s/%%FAMILY%%/$task_family/g" 
+sed -i -e $replace $generated_dir/task-definition.json
 
 
 
