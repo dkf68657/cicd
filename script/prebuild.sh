d3d03b52767f205ea58537e860b45c665ad77c94
brucedong <542214695@qq.com>
2020-05-06 13:39:09 +0800

    wwwwww

--- script/prebuild.sh
+++ script/prebuild.sh
@@ -7,13 +7,12 @@ rm -rf $generated_dir
 echo "mkdir -p $generated_dir"  
 mkdir -p $generated_dir
 
-cp -r $template_dir   $generated_dir
-
+cp -r $template_dir/*   $generated_dir
 
 echo "replace placehold in $generated_dir/task-definition.json"  
-family = cat config.json | jq .task.family
-replace="s/%%FAMILY%%/$1/g" 
-sed -i -e $replace $generated_dir/task-definition.json
+family = "cat config.json | jq .task.family"
+replace= "s/%%FAMILY%%/$family/g" 
+sed -i -e $replace "$generated_dir/task-definition.json"
 
 
 
