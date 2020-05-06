2eb8744e7032ea8d452ea548f32da49b010bc8f3
brucedong <542214695@qq.com>
2020-05-06 20:21:40 +0800

    weeeeeeeeeeeeeeeee

--- jenkins.sh
+++ jenkins.sh
@@ -4,6 +4,7 @@ chmod 776 build.sh
 ./prebuild.sh
 ./build.sh $CLUSTER $servicename $REGION $imageUrl
 echo Deploying Docker image to AWS Fargate on `date`
+echo "ecs-deploy -c $CLUSTER -n $servicename  -r $REGION -p default  -i $imageUrl"
 chmod 766 ecs-deploy
 ./ecs-deploy -c $CLUSTER -n $servicename  -r $REGION -p default  -i $imageUrl
 echo start to deploy aws farget successfully
--- prebuild.sh
+++ prebuild.sh
@@ -103,7 +103,7 @@ echo "aws ecs describe-services --service $servicename --cluster $CLUSTER --regi
 
 SERVICES=`aws ecs describe-services --service $servicename --cluster $CLUSTER --region $REGION | jq .failures[]`
 
-if [ "$?" -ne "0"]; then
+if [ $? -ne 0]; then
  echo failed to get service info
  exit 1;
 
@@ -111,5 +111,5 @@ if [ "$SERVICES"==""]; then
   echo "service has existed"
 else
   echo "create a new service"
-  aws ecs create-service cli-input-json file://$generated_dir/service-definition.json
+  aws ecs create-service --cli-input-json file://$generated_dir/service-definition.json
 fi  
