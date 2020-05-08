31b616f858af94c9d4f7ba217d8b134d9a23ed4c
brucedong <542214695@qq.com>
2020-05-08 19:43:38 +0800

    eeeeeeeeeeeee

--- src/test/java/com/cicd/demo/CicdApplicationTests.java
+++ src/test/java/com/cicd/demo/CicdApplicationTests.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.concurrent.TimeUnit;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,9 +17,29 @@ class CicdApplicationTests {
 	@Autowired
     private RestTemplate restTemplate = new RestTemplate();
 	@Test
-	void contextLoads() throws Exception, URISyntaxException {
-		ResponseEntity<String> response = restTemplate.getForEntity(new URI(lb +"helloworld"), String.class);
-		assertTrue(response.getBody().startsWith("welcome to hello world for springboot"));
+	void contextLoads(){
+		 int count = 0;
+		 boolean success =true;
+		while(true) {
+			if(count < 10)
+			try {
+				ResponseEntity<String> response = restTemplate.getForEntity(new URI(lb +"helloworld"), String.class);
+				assertTrue(response.getBody().startsWith("welcome to hello world for springboot"));
+			}catch(Exception e) {
+				count ++;
+				success = false;
+			}
+			if(success) {
+				return;
+			}
+			
+			try {
+				TimeUnit.SECONDS.sleep(30);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		
 	}
 
 }
